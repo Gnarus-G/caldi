e0990a3aff349332055f23ff53ba35c54c89f8ea
gnarus-g <37311893+Gnarus-G@users.noreply.github.com>
2023-12-09 15:18:52 -0500

    feat: setting up interface for a repl and eval function

---
+++ src/calc/mod.rs
@@ -0,0 +1,5 @@
+mod parse;
+
+pub fn eval(source: &str) -> String {
+    return "3".to_string();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,38 @@
-use std::sync::{Arc, Condvar, Mutex};
+use std::{
+    io::{stdin, stdout, Write},
+    sync::{Arc, Condvar, Mutex},
+};
 
+mod calc;
 mod stt;
 
-use clap::Parser;
+use calc::eval;
+use clap::{Args, Parser, Subcommand};
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 use ringbuf::{LocalRb, Rb};
 use tts::Tts;
 
 #[derive(Parser)]
 struct CLi {
-    #[clap(default_value = "Caldi")]
+    #[clap(subcommand)]
+    command: Option<Command>,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    Assistant(AssistantInterface),
+}
+
+#[derive(Args)]
+struct AssistantInterface {
+    #[clap(long = "name", default_value = "Caldi")]
     assistant_name: String,
 }
 
-impl CLi {
+impl AssistantInterface {
+    const WHISPER_SAMPLE_RATE: u32 = 16000;
+    const WHISPER_CHANNEL_COUNT: u16 = 1; // mono because whisper wants it
+
     fn waiting_mode_transcription_prompt(&self) -> String {
         format!(
             r#"[system] The user will probably say "Hey, {}", and if they don't then just repeat what they said. [user]"#,
@@ -25,10 +44,125 @@ impl CLi {
         let text = text.trim().to_lowercase();
         return text.starts_with("hey") && text.contains(&self.assistant_name.to_lowercase());
     }
-}
 
-const WHISPER_SAMPLE_RATE: u32 = 16000;
-const WHISPER_CHANNEL_COUNT: u16 = 1; // mono because whisper wants it
+    fn handle(self) -> anyhow::Result<()> {
+        let tts = Arc::new(Mutex::new(Tts::default()?));
+        let _tts = Arc::clone(&tts);
+
+        let host = cpal::default_host();
+
+        let device = host
+            .default_input_device()
+            .expect("failed to get input device");
+
+        let audio_input_buffer_size = Self::WHISPER_SAMPLE_RATE * 2; // going for a buffer spanning 2 seconds
+
+        // We'll try and use the same configuration between streams to keep it simple.
+        let config: cpal::StreamConfig = cpal::StreamConfig {
+            channels: Self::WHISPER_CHANNEL_COUNT,
+            sample_rate: cpal::SampleRate(Self::WHISPER_SAMPLE_RATE),
+            buffer_size: cpal::BufferSize::Fixed(audio_input_buffer_size),
+        };
+
+        let mut waiting_audio = LocalRb::new(audio_input_buffer_size as usize * 2);
+
+        let speech_audio = Arc::new(Mutex::new(Vec::<f32>::new()));
+        let _speech_audio = Arc::clone(&speech_audio);
+
+        let signal = Arc::new((Mutex::new(ListenState::Waiting), Condvar::new()));
+        let _signal = Arc::clone(&signal);
+
+        let tr = Arc::new(stt::Transcribe::new());
+        let _tr = Arc::clone(&tr);
+
+        let input_stream = device.build_input_stream(
+            &config,
+            move |data: &[f32], _info| {
+                let mut state = signal.0.lock().unwrap();
+
+                match *state {
+                    ListenState::Waiting => {
+                        waiting_audio.push_slice_overwrite(data);
+
+                        let (first, second) = waiting_audio.as_slices();
+                        let data = &[first, second].concat();
+
+                        if is_silence(data) {
+                            eprintln!("[INFO] silence detected, still waiting");
+                            return;
+                        }
+
+                        let text = _tr.transcribe(data, &self.waiting_mode_transcription_prompt());
+
+                        eprintln!("[DEBUG] heard and transcribed: {}", text);
+                        if self.is_signal_to_start_command(&text) {
+                            eprintln!(
+                                "[DEBUG] received signal to start recording command: {}",
+                                &text
+                            );
+
+                            eprintln!("[INFO] recording...");
+
+                            _tts.lock()
+                                .unwrap()
+                                .speak("Ready!", false)
+                                .expect("failed to speak");
+
+                            *state = ListenState::Listening;
+                            waiting_audio.clear();
+                        } else if !text.to_lowercase().trim_start().starts_with("hey") {
+                            waiting_audio.clear();
+                        }
+                    }
+                    ListenState::Listening => {
+                        let mut s = _speech_audio.lock().unwrap();
+                        for &sample in data {
+                            s.push(sample);
+                        }
+
+                        if is_silence(data) && !is_silence(&s) {
+                            eprintln!("[INFO] silence detected after having spoken something");
+                            *state = ListenState::Transcribing;
+                            let (_, cvar) = &*signal;
+                            cvar.notify_one();
+                        }
+                    }
+                    ListenState::Transcribing => {
+                        eprintln!("[DEBUG] noop in input_stream, currently transcribing");
+                    }
+                }
+            },
+            err_fn,
+            None,
+        )?;
+
+        input_stream.play()?;
+
+        loop {
+            let (_state, cvar) = &*_signal;
+            let mut state = _state.lock().unwrap();
+
+            while *state != ListenState::Transcribing {
+                state = cvar.wait(state).unwrap();
+            }
+
+            input_stream.pause()?;
+
+            let mut data = speech_audio.lock().unwrap();
+
+            let prompt = r#"[system] Get ready. The user will pose some math problems. [user]"#;
+            let text = tr.transcribe(&data, prompt);
+            let text = eval(&text);
+
+            println!("[answer]: {text}");
+            tts.lock().unwrap().speak(text, false)?;
+
+            *state = ListenState::Waiting;
+            data.clear();
+            input_stream.play()?;
+        }
+    }
+}
 
 #[derive(Debug, PartialEq)]
 enum ListenState {
@@ -39,122 +173,25 @@ enum ListenState {
 
 fn main() -> Result<(), anyhow::Error> {
     let cli = CLi::parse();
-    let tts = Arc::new(Mutex::new(Tts::default()?));
-    let _tts = Arc::clone(&tts);
 
-    let host = cpal::default_host();
+    match cli.command {
+        Some(Command::Assistant(a)) => a.handle()?,
+        None => {
+            print!(":> ");
+            stdout().flush()?;
+            for _line in stdin().lines() {
+                let line = _line?;
 
-    let device = host
-        .default_input_device()
-        .expect("failed to get input device");
+                let answer = eval(&line);
 
-    let audio_input_buffer_size = WHISPER_SAMPLE_RATE * 2; // going for a buffer spanning 2 seconds
-
-    // We'll try and use the same configuration between streams to keep it simple.
-    let config: cpal::StreamConfig = cpal::StreamConfig {
-        channels: WHISPER_CHANNEL_COUNT,
-        sample_rate: cpal::SampleRate(WHISPER_SAMPLE_RATE),
-        buffer_size: cpal::BufferSize::Fixed(audio_input_buffer_size),
-    };
-
-    let mut waiting_audio = LocalRb::new(audio_input_buffer_size as usize * 2);
-
-    let speech_audio = Arc::new(Mutex::new(Vec::<f32>::new()));
-    let _speech_audio = Arc::clone(&speech_audio);
-
-    let signal = Arc::new((Mutex::new(ListenState::Waiting), Condvar::new()));
-    let _signal = Arc::clone(&signal);
-
-    let tr = Arc::new(stt::Transcribe::new());
-    let _tr = Arc::clone(&tr);
-
-    let input_stream = device.build_input_stream(
-        &config,
-        move |data: &[f32], _info| {
-            let mut state = signal.0.lock().unwrap();
-
-            match *state {
-                ListenState::Waiting => {
-                    waiting_audio.push_slice_overwrite(data);
-
-                    let (first, second) = waiting_audio.as_slices();
-                    let data = &[first, second].concat();
-
-                    if is_silence(data) {
-                        eprintln!("[INFO] silence detected, still waiting");
-                        return;
-                    }
-
-                    let text = _tr.transcribe(data, &cli.waiting_mode_transcription_prompt());
-
-                    eprintln!("[DEBUG] heard and transcribed: {}", text);
-                    if cli.is_signal_to_start_command(&text) {
-                        eprintln!(
-                            "[DEBUG] received signal to start recording command: {}",
-                            &text
-                        );
-
-                        eprintln!("[INFO] recording...");
-
-                        _tts.lock()
-                            .unwrap()
-                            .speak("Ready!", false)
-                            .expect("failed to speak");
-
-                        *state = ListenState::Listening;
-                        waiting_audio.clear();
-                    } else if !text.to_lowercase().trim_start().starts_with("hey") {
-                        waiting_audio.clear();
-                    }
-                }
-                ListenState::Listening => {
-                    let mut s = _speech_audio.lock().unwrap();
-                    for &sample in data {
-                        s.push(sample);
-                    }
-
-                    if is_silence(data) && !is_silence(&s) {
-                        eprintln!("[INFO] silence detected after having spoken something");
-                        *state = ListenState::Transcribing;
-                        let (_, cvar) = &*signal;
-                        cvar.notify_one();
-                    }
-                }
-                ListenState::Transcribing => {
-                    eprintln!("[DEBUG] noop in input_stream, currently transcribing");
-                }
+                println!("{answer}");
+                print!(":> ");
+                stdout().flush()?;
             }
-        },
-        err_fn,
-        None,
-    )?;
-
-    input_stream.play()?;
-
-    loop {
-        let (_state, cvar) = &*_signal;
-        let mut state = _state.lock().unwrap();
-
-        while *state != ListenState::Transcribing {
-            state = cvar.wait(state).unwrap();
         }
-
-        input_stream.pause()?;
-
-        let mut data = speech_audio.lock().unwrap();
-
-        let prompt = r#"[system] Get ready. The user will pose some math problems. [user]"#;
-        let text = tr.transcribe(&data, prompt);
-
-        println!("[echo]: {text}");
-        tts.lock()
-            .unwrap()
-            .speak(&format!("I heard you say, {text}"), false)?;
-
-        *state = ListenState::Waiting;
-        data.clear();
-        input_stream.play()?;
     }
+
+    return Ok(());
 }
 
 fn is_silence(samples: &[f32]) -> bool {
